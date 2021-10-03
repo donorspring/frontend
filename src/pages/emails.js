4413542529edc8dba43538b4b373a5053e2e7db9
cls291 <cls291@georgetown.edu>
2021-10-03 15:30:22 -0400

    switched emails and uploads page

--- src/pages/emails.js
+++ src/pages/emails.js
@@ -1,10 +1,11 @@
 import React from 'react'; 
-import ListUpload from '../components/listUpload'
 
 
 function Emails() {
     return (
-      <ListUpload></ListUpload>
+      <p>
+        Emails page
+        </p>
     );
   }
 
