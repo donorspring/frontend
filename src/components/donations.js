ee3a2b7e4db7beb0ff919f1d686305474904df9e
cls291 <cls291@georgetown.edu>
2021-11-02 09:10:13 -0400

    saving changes

--- src/components/donations.js
+++ src/components/donations.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Card from "@material-ui/core/Card";
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography'; 
-import CardActions from '@material-ui/core/CardActions'; 
-import Button from '@material-ui/core/Button'; 
 import Box from '@material-ui/core/Box'; 
 
 const card = (
